4b11c657eb6dd9939e1c820a583327c8eb7021c9
Alan-Nasciment0 <alandonascimento2014@gmail.com>
2025-10-07 19:39:20 -0300

    formatando a pgManterLivro.php

--- paginas_tcc/pgManterLivro.php
+++ paginas_tcc/pgManterLivro.php
@@ -20,29 +20,44 @@
             </search>
 
             <div class="containerInformacoesLivro">
-                <div style="display: flex;">
+                <div style="display: flex; gap: 8.31rem; margin-top: 6.56rem;">
                     <div class="adicionarCapaLivro">
-
+                        <input type="file">
                     </div>
                     <div class="informacoesLivro">
-                        <div>
-
+                        <div class="containerCampoPreencher">
+                            <label>Nome do Livro</label>
+                            <div class="campoPreencher"> 
+                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite o nome do livro" class="text-body-secondary">
+                            </div>
                         </div>
 
-                        <div>
-
+                        <div class="containerCampoPreencher">
+                            <label>Autor do Livro</label>
+                            <div class="campoPreencher"> 
+                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite o Autor" class="text-body-secondary">
+                            </div>
                         </div>
 
-                        <div>
-
+                        <div class="containerCampoPreencher">
+                            <label>Gênero do livro</label>
+                            <div class="campoPreencher"> 
+                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite o gênero" class="text-body-secondary">
+                            </div>
                         </div>
 
-                        <div>
-
+                        <div class="containerCampoPreencher">
+                            <label>Ano de publicação</label>
+                            <div class="campoPreencher"> 
+                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite o ano" class="text-body-secondary">
+                            </div>
                         </div>
 
-                        <div>
-
+                        <div class="containerCampoPreencher">
+                            <label>Editora do livro</label>
+                            <div class="campoPreencher"> 
+                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite a editora" class="text-body-secondary">
+                            </div>
                         </div>
                     </div>
                 </div>
