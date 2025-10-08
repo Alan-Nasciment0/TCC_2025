47dee80351b1b597d47b651e04dc7f2aa210f70d
Alan-Nasciment0 <alandonascimento2014@gmail.com>
2025-10-07 21:38:46 -0300

    formatando a pgManterLivro.php

--- paginas_tcc/pgManterLivro.php
+++ paginas_tcc/pgManterLivro.php
@@ -22,44 +22,54 @@
             <div class="containerInformacoesLivro">
                 <div style="display: flex; gap: 8.31rem; margin-top: 6.56rem;">
                     <div class="adicionarCapaLivro">
-                        <input type="file">
+                        <label for="file-upload" class="custom-file-label"><img src="../img/nuvem.png"></label>
+                        <label for="file-upload" class="custom-file-label">Adicionar Capa do Livro</label>
+                        <input type="file" id="file-upload" style="display: none;">
                     </div>
                     <div class="informacoesLivro">
                         <div class="containerCampoPreencher">
                             <label>Nome do Livro</label>
                             <div class="campoPreencher"> 
-                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite o nome do livro" class="text-body-secondary">
+                                <input class="placeHolder" type="text" id="nomeLivro" name="nomeLivro" placeholder="Digite o nome do livro" class="text-body-secondary">
                             </div>
                         </div>
 
                         <div class="containerCampoPreencher">
                             <label>Autor do Livro</label>
                             <div class="campoPreencher"> 
-                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite o Autor" class="text-body-secondary">
+                                <input class="placeHolder" type="text" id="autor" name="autor" placeholder="Digite o Autor" class="text-body-secondary">
                             </div>
                         </div>
 
                         <div class="containerCampoPreencher">
                             <label>Gênero do livro</label>
                             <div class="campoPreencher"> 
-                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite o gênero" class="text-body-secondary">
+                                <input class="placeHolder" type="text" id="genero" name="genero" placeholder="Digite o gênero" class="text-body-secondary">
                             </div>
                         </div>
 
                         <div class="containerCampoPreencher">
                             <label>Ano de publicação</label>
                             <div class="campoPreencher"> 
-                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite o ano" class="text-body-secondary">
+                                <input class="placeHolder" type="text" id="anoPubli" name="anoPubli" placeholder="Digite o ano" class="text-body-secondary">
                             </div>
                         </div>
 
                         <div class="containerCampoPreencher">
                             <label>Editora do livro</label>
                             <div class="campoPreencher"> 
-                                <input class="placeHolder" type="text" id="email" name="email" placeholder="Digite a editora" class="text-body-secondary">
+                                <input class="placeHolder" type="text" id="editora" name="editora" placeholder="Digite a editora" class="text-body-secondary">
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div style="margin-top: 1.87rem;">
+                    <label>Descrição</label>
+                            <div class="campoDescricao"> 
+                                <input class="placeHolder" type="text" id="descricao" name="descricao" placeholder="Digite a Descrição do livro" size="400" style="width: 59.68rem;">
+                            </div>
+                            <button type="button" class="btn btn-dark" style="float: right; margin-top: 1.87rem;">Adicionar Livro</button>
                 </div>
             </div>
         </div>
