87457305b918317e362bb217b63560625ab6e298
Arthur <arthurnascimento1404@gmail.com>
2025-10-10 16:03:20 -0300

    ADD ranking

---
+++ paginas_tcc/pgRanking.php
@@ -0,0 +1,68 @@
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Ranking de Livros</title>
+      <link rel="stylesheet" href="../css_js/css/ranking.css">
+
+    </head>
+    <body>
+      <div class="tela">
+
+        <div class="barra">
+          <img src="../img/Quanto_mais_você_lê_mais_você_voa__2_-removebg-preview.png" alt="Logo">
+          <div class="menu">
+            <h4>Início</h4>
+            <h4>Ranking</h4>
+            <div class="search-box">
+              <input type="text" placeholder="Search">
+              <button>Pesquisar</button>
+            </div>
+            <span>Favoritos</span>
+          </div>
+        </div>
+
+        <div class="filtros">
+          <ul>
+            <li><a href="#">Melhores avaliados ></a></li>
+            <li><a href="#">Livros de histórias curtas ></a></li>
+            <li><a href="#">Livros de histórias grandes ></a></li>
+          </ul>
+        </div>
+
+       <div class="card">
+  <div class="capa">
+    <img src="../img/sitio_do_picapauamarelo.jpg" class="capaimg">
+  </div>
+  <div class="info">
+    <div class="titulo">1. A droga da obediência</div>
+    <div class="autor">Pedro Bandeira</div>
+    <div class="avaliacao">⭐ 4.5 (100 avaliações)</div>
+  </div>
+</div>
+
+
+          <div class="card">
+            <div class="capa"></div>
+            <img src="../img/Reinações_de_Narizinho.webp" class="capaimg">
+            <div class="info">
+              <div class="titulo">2. O Senhor dos Anéis</div>
+              <div class="autor">J.R.R. Tolkien</div>
+              <div class="avaliacao">⭐ 5.0 (200 avaliações)</div>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="capa"></div>
+            <img src="../img/Reinações_de_Narizinho.webp" class="capaimg">
+            <div class="info">
+              <div class="titulo">3. Dom Casmurro</div>
+              <div class="autor">Machado de Assis</div>
+              <div class="avaliacao">⭐ 4.8 (150 avaliações)</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+    </html>
