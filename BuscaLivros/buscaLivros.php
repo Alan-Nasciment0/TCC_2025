e02ba82a02553c4d7b7d9f631d5269da9b3bd5d3
Alan-Nasciment0 <alandonascimento2014@gmail.com>
2025-10-27 21:36:55 -0300

    mostrando informações completa dos livros apresentados na home, adicionando codigo para selecionar o livro e puxar as informações do livro selecionado

--- BuscaLivros/buscaLivros.php
+++ BuscaLivros/buscaLivros.php
@@ -8,10 +8,44 @@ $options = [
 
 try {
 
-  // Consulta todos os produtos
-  $stmt = $pdo->query("SELECT * FROM livros");
-  $livros = $stmt->fetchAll();
+  // Consulta todos os livros
+  $sql = "SELECT 
+    l.livro_cod,
+    l.livro_titulo,
+    l.livro_capa_link,
+    a.autor_nome
+FROM livros l
+LEFT JOIN autorLivro al ON l.livro_cod = al.livro_cod
+LEFT JOIN autor a ON al.autor_cod = a.autor_cod
+ORDER BY l.livro_titulo ";
+    
+  $stmt = $pdo->query($sql);
+  $livrosRaw = $stmt->fetchAll();
 
+    // Organiza os dados para agrupar múltiplos autores por livro
+    $informacoes_livros = [];
+
+    foreach ($livrosRaw as $row) {
+        $livroCod = $row['livro_cod'];
+
+        if (!isset($informacoes_livros[$livroCod])) {
+            $informacoes_livros[$livroCod] = [
+                'livro_cod' => $row['livro_cod'],
+                'livro_titulo' => $row['livro_titulo'],
+                'livro_capa_link' => $row['livro_capa_link'],
+                'autores' => [], // array para múltiplos autores
+            ];
+        }
+
+        if ($row['autor_nome']) {
+            $informacoes_livros[$livroCod]['autores'][] = $row['autor_nome'];
+        }
+    }
+    foreach ($informacoes_livros as &$livro) {
+    $livro['autor_nome'] = implode(', ', $livro['autores']); // junta os autores em uma string
+    }
+    unset($livro);
+ 
 } catch (PDOException $e) {
   die("Erro ao conectar ao banco de dados: " . $e->getMessage());
 }
--- componentes/componentesPaginas_tcc/livrosRecomendados.php
+++ componentes/componentesPaginas_tcc/livrosRecomendados.php
@@ -1,17 +1,25 @@
-<?php if (count($livros) > 0): ?>
-            <?php foreach ($livros as $livro): ?>
-            <div class="livro">
-                <img src="<?= htmlspecialchars($livro['livro_capa_link']) ?>" class="imgLivro">
-                <div class="gradiente"></div>
-                <a class="marcador"><img src="../img/salvar_livro.png" class="imgMarcador"></a>
-                <h6 class="nomeLivro">
-                    <?= htmlspecialchars($livro['livro_titulo']) ?>
-                </h6>
-                <h6 class="nomeAutor">Machado de Assis</h6>
-                <div class="avaliacoes">
-                    <img src="../img/star.png" class="imgEstrela">
-                    <h6 class="mediaAvaliacao">4,1</h6>
-                </div>
-            </div>
-            <?php endforeach; ?>
-            <?php endif; ?>
+<?php if (count($informacoes_livros) > 0): ?>
+<?php foreach ($informacoes_livros as $info_livro): ?>
+
+    <div class="livro">
+        <img src="<?= htmlspecialchars($info_livro['livro_capa_link']) ?>" class="imgLivro">
+        <div class="gradiente"></div>
+        <a class="marcador"><img src="../img/salvar_livro.png" class="imgMarcador"></a>
+        <h6 class="nomeLivro">
+            <?= htmlspecialchars($info_livro['livro_titulo']) ?>
+        </h6>
+        <h6 class="nomeAutor">
+            <?= htmlspecialchars($info_livro['autor_nome']) ?>
+        </h6>
+        <div class="avaliacoes">
+            <img src="../img/star.png" class="imgEstrela">
+            <h6 class="mediaAvaliacao">4,1</h6>
+        </div>
+        <form name="form_pgLivro" action="pgLivro.php" method="post">
+        <input type="hidden" name="cod_livro_selecionado" value="$info_livro['livro_cod']">
+        <input type="submit" class="botaoLivroSelecionado" name="livro_selecionado" value="">
+    </form>
+    </div>
+    
+<?php endforeach; ?>
+<?php endif; ?>
