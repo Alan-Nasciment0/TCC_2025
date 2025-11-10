37c61a3949c626c50e5fea54f9aa078e7478e935
edukir099 <meireleseduardo15@gmail.com>
2025-11-10 20:28:28 -0300

    Realização das paginas de denuncia

---
+++ componentes/pgDenuncia.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="stylesheet" href="../css_js/css/styleDenuncia.css">
+    <link rel="stylesheet" href="../css_js/bootstrap/css/bootstrap.min.css">
+    <script src="../css_js/bootstrap/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página denuncia</title>
+</head>
+
+<body>
+    <div class="containerPrincipal">
+        <div class="containerDenunciar">
+            <h2 class="titulo">Denunciar comentário</h2>
+            <div class="form-check" style="margin-bottom: .400rem;">
+                <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault1">
+                <label class="form-check-label" for="radioDefault1">
+                    Discurso de ódio
+                </label>
+            </div>
+            <div class="form-check" style="margin-bottom: .400rem;">
+                <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault2">
+                <label class="form-check-label" for="radioDefault2">
+                    Violencia explícita
+                </label>
+            </div>
+            <div class="form-check" style="margin-bottom: .400rem;">
+                <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault3">
+                <label class="form-check-label" for="radioDefault3">
+                    Assédio ou bullying
+                </label>
+            </div>
+            <div class="form-check" style="margin-bottom: .400rem;">
+                <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault4">
+                <label class="form-check-label" for="radioDefault4">
+                    Spam
+                </label>
+            </div>
+            <div class="form-check" style="margin-bottom: .400rem;">
+                <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault5">
+                <label class="form-check-label" for="radioDefault5">
+                    Outros
+                </label>
+            </div>
+            <div class="botoes">
+                <input type="submit" name="cancelar" value="Cancelar" style="color: black; background-color: white;">
+                <input type="submit" name="pgDescrição" value="Continuar">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ componentes/pgDenunciaDes.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="../css_js/css/styleDenunciaDes.css">
+    <link rel="stylesheet" href="../css_js/bootstrap/css/bootstrap.min.css">
+    <script src="../css_js/bootstrap/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página descrição da denuncia</title>
+</head>
+
+<body>
+    <div class="containerPrincipal">
+        <div class="containerDescrição">
+            <h2 class="titulo">Denunciar comentario</h2>
+            <h3 class="descrição">Forneça mais detalhes:</h3>
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
+                    style="height: 144px;"></textarea>
+                <label for="floatingTextarea2">Comente aqui</label>
+            </div>
+            <div class="botoes">
+                <input type="submit" name="cancelar" value="Cancelar" style="color: black; background-color: white;">
+                <input type="submit" name="pgPaginadoLivro" value="Enviar denuncia">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
