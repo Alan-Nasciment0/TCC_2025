c92746e76873d557ccae3925c76b935600bfc3a3
Alan-Nasciment0 <alandonascimento2014@gmail.com>
2025-11-25 22:18:43 -0300

    corrigindo paginas

--- componentes/componentesPaginas_tcc/buscaBanner.php
+++ componentes/componentesPaginas_tcc/buscaBanner.php
@@ -4,7 +4,7 @@
 
     <?php foreach ($banners_ativos as $index => $banner): ?>
         <div class="carousel-item <?= $index === 0 ? 'active' : '' ?>">
-            <a href="<?= htmlspecialchars($banner['link']) ?>">
+            <a href="<?= htmlspecialchars($banner['link']) ?>" target="_blank">
                 <img class="d-block w-100 bannerIMG" 
                      src="../img/banners/<?= htmlspecialchars($banner['banner_imagem']) ?>" 
                      alt="Banner">
